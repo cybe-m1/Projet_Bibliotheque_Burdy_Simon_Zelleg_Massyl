d84a37d628bf82c8e0f8ff824cec3973a3e32ec5
SimonBurdy <simon.burdy@lacatholille.fr>
2022-04-26 21:18:36 +0200

    "🔨 add comment service"

---
+++ comment-service/comment-service/src/main/java/com/fges/comment/controller/CommentController.java
@@ -0,0 +1,54 @@
+package com.fges.comment.controller;
+
+import java.util.List;
+
+import com.fges.comment.CommentNotFoundException;
+import com.fges.comment.entity.Comment;
+import com.fges.comment.service.CommentService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/comments")
+public class CommentController {
+    @Autowired
+    private CommentService commentService;
+
+    @PostMapping
+    public Comment saveComment( @RequestBody Comment comment){
+        return commentService.saveComment(comment);
+    }
+    @GetMapping
+    public List<Comment> getAll(){
+        return commentService.getAll();
+    }
+
+    @GetMapping("/id/{commentId}")
+    public Comment getCommentById(@PathVariable("commentId") Long commentId ) throws Exception{
+        return commentService.getCommentById(commentId);
+    }
+
+    @GetMapping("/commentTitle/{commentName}")
+    public Comment getCommentByName(@PathVariable String commentTitle) throws Exception {
+        return commentService.getCommentByTitle(commentTitle);
+    }
+
+    @PutMapping
+    public Comment update(@RequestBody Comment comment) throws CommentNotFoundException {
+        return commentService.updateComment(comment);
+    }
+
+    @DeleteMapping(value="/delete/{commentId}")
+    public Comment delete(@PathVariable Long commentId) throws Exception {
+        Comment toDelete = commentService.getCommentById(commentId);
+        commentService.deleteCommentById(commentId);
+        return toDelete;
+    }
+
+}
