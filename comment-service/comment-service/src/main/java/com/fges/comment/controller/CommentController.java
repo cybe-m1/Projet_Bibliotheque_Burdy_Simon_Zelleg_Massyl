7c9681eb3a9ac0cf204c4d520c7b41d2404d0fcc
zelleg <massyl.zelleg2201@gmail.com>
2022-04-28 23:24:27 +0200

    ✅ restore correct version

--- comment-service/comment-service/src/main/java/com/fges/comment/controller/CommentController.java
+++ comment-service/comment-service/src/main/java/com/fges/comment/controller/CommentController.java
@@ -7,14 +7,9 @@ import com.fges.comment.entity.Comment;
 import com.fges.comment.service.CommentService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
 @RequestMapping("/comments")
 public class CommentController {
     @Autowired
@@ -34,10 +29,10 @@ public class CommentController {
         return commentService.getCommentById(commentId);
     }
 
-    @GetMapping("/commentTitle/{commentName}")
-    public Comment getCommentByName(@PathVariable String commentTitle) throws Exception {
-        return commentService.getCommentByTitle(commentTitle);
-    }
+    //@GetMapping("/commentTitle/{commentName}")
+    //public Comment getCommentByName(@PathVariable String commentTitle) throws Exception {
+    //  return commentService.getCommentByTitle(commentTitle);
+    //}
 
     @PutMapping
     public Comment update(@RequestBody Comment comment) throws CommentNotFoundException {
