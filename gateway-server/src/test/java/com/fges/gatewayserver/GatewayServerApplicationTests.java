11c4f7da6f4e2f2892fc0ef0d26f4676dd8a4aa9
zelleg <massyl.zelleg2201@gmail.com>
2022-04-29 20:23:43 +0200

    ➕ add gateway ➕

---
+++ gateway-server/src/test/java/com/fges/gatewayserver/GatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fges.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
