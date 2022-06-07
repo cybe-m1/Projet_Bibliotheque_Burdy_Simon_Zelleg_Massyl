1e28a4de0ec70f8e253807101e53d07e71918168
zelleg <massyl.zelleg2201@gmail.com>
2022-06-07 17:54:12 +0200

    correct method leave comment

--- comment-service/src/main/java/com/fges/comment/controller/CommentController.java
+++ comment-service/src/main/java/com/fges/comment/controller/CommentController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import com.fges.comment.CommentNotFoundException;
 import com.fges.comment.UserOrBookNotFoundException;
@@ -12,10 +13,12 @@ import com.fges.comment.entity.Comment;
 import com.fges.comment.entity.UserDTO;
 import com.fges.comment.service.CommentService;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @RestController
 @RequestMapping("/comments")
 public class CommentController {
@@ -27,16 +30,17 @@ public class CommentController {
 
     @PostMapping
     public Comment saveComment(@RequestBody Comment comment) throws UserOrBookNotFoundException {
-        //check if book and user exists before creating comment
         Long bookId = comment.getBookId();
         Long userId = comment.getUserId();
         BookDTO bookMatching = restTemplate.getForObject("http://BOOK-SERVICE/books/id/"+ bookId, BookDTO.class);
+        String nameMatching = bookMatching.getName();
         UserDTO userMatching = restTemplate.getForObject("http://USER-SERVICE/api/id/"+ userId, UserDTO.class);
-        Object[] history = restTemplate.getForObject("http://BOOK-SERVICE/books/userId/" + userId + "/history", Object[].class);
-        List<Object> historyList = Arrays.asList(history);
+        BookDTO[] history = restTemplate.getForObject("http://BOOK-SERVICE/books/userId/" + userId + "/history", BookDTO[].class);
+        List<BookDTO> historyList = Arrays.asList(history);
         if((bookMatching instanceof BookDTO) && (userMatching instanceof UserDTO)) {
-            if(historyList.contains(bookMatching)){
-                return commentService.saveComment(comment);
+            List<Boolean> matchingList = historyList.stream().map(sc -> sc.getBookId() == bookId).collect(Collectors.toList());
+            if(matchingList.contains(true)){
+                    return commentService.saveComment(comment);
             }
         }
         else {
--- comment-service/src/main/java/com/fges/comment/entity/BookDTO.java
+++ comment-service/src/main/java/com/fges/comment/entity/BookDTO.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Map;
+
 @Data
 @Getter
 @Setter
@@ -13,4 +15,5 @@ public class BookDTO {
     private Long bookId;
     private String name;
     private String category;
+    Map<Long, Boolean> usersIds;
 }
